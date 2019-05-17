58670fe53823039040f912d2c07dfb17e56115db
pawel <pawel.klejka12653@gmail.com>
2019-05-17 11:52:27 +0200

    added sign in/out button in header

--- sleep-go-react/src/components/shared/Header/Header.js
+++ sleep-go-react/src/components/shared/Header/Header.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Navbar, Nav, Form, Button, FormControl} from 'react-bootstrap/';
 import {Link} from 'react-router-dom';
 
 
-const header = () =>{
-    return(
-    <Navbar bg="dark" variant="dark">
-    <Link to='/'><Navbar.Brand href="#home">SleepGo</Navbar.Brand></Link>
-    <Nav className="mr-auto">
-        <Link to='/'><Nav.Link href="#home">Home</Nav.Link></Link>
-        <Link to='/login'><Nav.Link href="#features">Login</Nav.Link></Link>
-        <Link to='/register'><Nav.Link href="#pricing">Register</Nav.Link></Link>
-    </Nav>
-    <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-danger">Search</Button>
-    </Form>
-    </Navbar>
-    )
+class Header extends Component{
+    state ={
+        signedIn: true
+    }
+    render(){
+        return(
+            <Navbar bg="dark" variant="dark">
+            <Link to='/'><Navbar.Brand href="#home">SleepGo</Navbar.Brand></Link>
+            <Nav className="mr-auto">
+                <Link to='/'><Nav.Link href="#home">Home</Nav.Link></Link>
+                <Link to='/login'><Nav.Link href="#features">Login</Nav.Link></Link>
+                <Link to='/register'><Nav.Link href="#pricing">Register</Nav.Link></Link>
+            </Nav>
+            <Form inline>
+                {this.state.signedIn ? <Button variant='light' className="mr-sm-2">Sign in</Button> : <Button variant='outline-light' className="mr-sm-2">Sign out</Button>}
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-danger">Search</Button>
+            </Form>
+            </Navbar>
+            )
+        }
 }
 
-export default header;
+export default Header;
