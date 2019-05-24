d6fbf7de2f40a91fcf99ba10277de0096dd74222
pawel <pawel.klejka12653@gmail.com>
2019-05-24 16:18:09 +0200

    fixed some bugs with forms, added error handlers

--- sleep-go-react/src/components/shared/Header/Header.js
+++ sleep-go-react/src/components/shared/Header/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import {Navbar, Nav, Form, Button, FormControl} from 'react-bootstrap/';
 import {Link} from 'react-router-dom';
 
 
-const header = (props) =>{
+class Header extends PureComponent{
+    
+    render(){
     return(
         <Navbar bg="dark" variant="dark">
         <Link to='/'><Navbar.Brand>SleepGo</Navbar.Brand></Link>
@@ -14,18 +16,19 @@ const header = (props) =>{
             <Link to='/register' className='nav-link'>Register</Link>
         </Nav>
         <Form inline>
-            {props.signedIn ? 
+            {this.props.signedIn ? 
             <Link to='/'>
-                <Button onClick={props.onSignOut} variant='outline-light' className="mr-sm-2">Sign out</Button>
+                <Button onClick={this.props.onSignOut} variant='outline-light' className="mr-sm-2">Sign out</Button>
             </Link> : 
             <Link to='/login'>
-                <Button onClick={props.onSignIn} variant='light' className="mr-sm-2">Sign in</Button>
+                <Button onClick={this.props.onSignIn} variant='light' className="mr-sm-2">Sign in</Button>
             </Link>}
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-danger">Search</Button>
         </Form>
         </Navbar>
         )
+    }
 }
 
-export default header;
+export default Header;
