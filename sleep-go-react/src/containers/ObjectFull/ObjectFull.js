274900a934d5932c5b09e7b4b2915f160dec246e
Gut Mateusz <gutmateusz96@interia.pl>
2019-05-25 14:38:38 +0200

    Added routing between basic card view to full object info

--- sleep-go-react/src/containers/ObjectFull/ObjectFull.js
+++ sleep-go-react/src/containers/ObjectFull/ObjectFull.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ReactHtmlParser from 'react-html-parser'; 
 
 import classes from './ObjectFull.css';
 
@@ -11,7 +12,7 @@ class ObjectFull extends Component {
     componentDidMount () {
         if ( this.props.match.params.id) {
             if ( !this.state.loadedObject || (this.state.loadedObject && this.state.loadedObject.id !== this.props.id) ) {
-                axios.get( 'https://sleep-go.firebaseio.com/objects.json/' + this.props.match.params.id )
+                axios.get( 'https://sleep-go.firebaseio.com/objects/' + this.props.match.params.id + '.json/' )
                     .then( response => {
                          console.log(response);
                         this.setState( { loadedObject: response.data } );
@@ -20,26 +21,27 @@ class ObjectFull extends Component {
         }
     }
 
-    deleteObjectHandler = () => {
-        axios.delete('https://sleep-go.firebaseio.com/objects.json/' + this.props.id)
+    /*  deleteObjectHandler = () => {
+        axios.delete('https://sleep-go.firebaseio.com/objects/' + this.props.id + '.json/' )
             .then(response => {
                 console.log(response);
             });
-    }
+    } */
 
     render () {
-        let object = <p style={{ textAlign: 'center' }}>Wybierz obiekt</p>;
+        let object = <p></p>;
         if ( this.props.id ) {
-            object = <p style={{ textAlign: 'center' }}>Wczytywanie</p>;
+            object = <p style={{ textAlign: 'center' }}>Wczytywanie...</p>;
         }
         if ( this.state.loadedObject ) {
             object = (
                 <div className={classes.ObjectFull}>
-                    <h1>{this.state.loadedObject.title}</h1>
-                    <p>{this.state.loadedObject.body}</p>
-                    <div className={classes.Edit}>
+                    <h1>{this.state.loadedObject.name}</h1>
+                    <p>{this.state.loadedObject.phone}</p>
+                    <p>{ ReactHtmlParser (this.state.loadedObject.description) }</p>
+                  {/*<div className={classes.Edit}>
                         <button onClick={this.deleteObjectHandler} className={classes.Delete}>Usuń</button>
-                    </div>
+            </div> */} 
                 </div>
 
             );
